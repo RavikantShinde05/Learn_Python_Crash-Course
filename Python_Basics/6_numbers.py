3239dfa13bbac3a73be7a850edac6997ec42cfc2
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2026-02-16 23:50:48 +0530

    Add files via upload

---
+++ Python_Basics/6_numbers.py
@@ -0,0 +1,28 @@
+# NUMBERS / INTEGERS:
+
+# In Python there are 2 types of number/integers.
+
+x = 1  # integer
+x = 2.5  # float
+# we do have
+# x = a + Bi # Complex numbers for
+# instead of "i" we use 'j"
+x = 1 + 2j  # used for math
+
+# OTHER OPERATIONS:
+
+print(10 + 2)  # output> 12
+print(10 - 2)  # output> 8
+print(10 * 2)  # output> 20
+print(10 / 2)  # output> 5.0
+print(10 / 3)  # output> 3.3333
+print(10 // 3)  # output> 3
+print(10 % 2)  # output> 0
+print(10 % 3)  # output> 1
+print(10 ** 2)  # output> 100
+
+# Augmented operation methods:
+x = 10
+y = x + 2
+# or
+y += 3
