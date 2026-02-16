fe0c019d75b1e1041197efb2978585d5277aba66
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2026-02-16 23:48:40 +0530

    Add files via upload

---
+++ Python_Basics/2_print_index_string.py
@@ -0,0 +1,25 @@
+# PRINT INDEX OF "STRING AND LENGTH" OF STRING
+
+university_name = "Harvard University"
+
+# email format in string variable
+email_message = """
+hi,
+this is David writting this mail to 
+thank you for the feedback
+"""
+# In built funtion to get String length
+# A function is a peace code which execute/performs or carries out a task and it is completely reusable.
+
+# GET Length of the String:
+
+print(len(university_name))  # output: 18
+
+# to access the charecters of string
+print(university_name[0])  # output: H
+print(university_name[2])  # output: r
+print(university_name[-1])  # output: y
+print(university_name[0:4])  # output: Harv
+print(university_name[0:])  # output: Harvard University
+print(university_name[:4])  # output: Harv
+print(university_name[:])  # output: Harvard University
