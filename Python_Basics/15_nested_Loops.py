ec49df735c710861cea6d4709a89da5d73ef4892
RevX05@ <117154752+RavikantShinde05@users.noreply.github.com>
2026-02-27 23:50:35 +0530

    Add files via upload

---
+++ Python_Basics/15_nested_Loops.py
@@ -0,0 +1,29 @@
+# Nested Loops:
+
+# for Loops:
+# print co-ordinates:
+
+for x in range(5):  # Outer Loop:
+    for y in range(3):  # Inner Loop:
+        print(f"({x}, {y})")
+
+# the interpretre iterates it by every elemenst:
+# like iterations goes from "x of 0 + y of 0".
+# them "x of 0 + y of 1".
+
+# output will be:
+# (0, 0)
+# (0, 1)
+# (0, 2)
+# (1, 0)
+# (1, 1)
+# (1, 2)
+# (2, 0)
+# (2, 1)
+# (2, 2)
+# (3, 0)
+# (3, 1)
+# (3, 2)
+# (4, 0)
+# (4, 1)
+# (4, 2)
